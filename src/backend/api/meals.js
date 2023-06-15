ea1df4efb3ee90970a7ccb00710a1b85c3f1fde9
hamoudi-hamoudi <mr-hamoudi@outlook.com>
2023-06-15 22:22:38 +0200

    nodejs week2 after review

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -12,78 +12,86 @@ const requiredColumns = [
   "price",
 ];
 
-router.get("/", async (req, res) => {
-  try {
-    const meals = await knex("meal").select("title");
-    res.status(200).json(meals);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-router.post("/", async (req, res) => {
-  const addMeal = req.body;
-  if (!valid(addMeal, requiredColumns)) {
-    return res.status(404).json("Missing required columns in the request body");
-  } else {
+router
+  .route("/")
+  .get(async (req, res) => {
     try {
-      await knex("meal").insert(addMeal);
-      res.status(201).json(`meal ${addMeal.title} added successfully`);
+      const meals = await knex("meal").select("title");
+      res.status(200).json(meals);
     } catch (err) {
       res.status(500).json(err);
     }
-  }
-});
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  if (isNaN(id)) {
-    res.status(404).json(` ${id} not valide please provide a number`);
-  } else {
-    try {
-      const meal = await knex("meal").select("title").where({ id });
-      meal.length === 0
-        ? res.status(200).json("no meal with this id")
-        : res.status(200).json(meal);
-    } catch (err) {
-      res.status(500).json(err);
+  })
+  .post(async (req, res) => {
+    const addMeal = req.body;
+    if (!valid(addMeal, requiredColumns)) {
+      return res
+        .status(404)
+        .json("Missing required columns in the request body");
+    } else {
+      try {
+        await knex("meal").insert(addMeal);
+        res.status(201).json(`meal ${addMeal.title} added successfully`);
+      } catch (err) {
+        res.status(500).json(err);
+      }
     }
-  }
-});
+  });
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id) || !id) {
+      res.status(404).json(` ${id} not valide please provide a number`);
+    } else {
+      try {
+        const meal = await knex("meal").select("title").where({ id });
+        meal.length === 0
+          ? res.status(200).json("no meal with this id")
+          : res.status(200).json(meal);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  })
 
-router.put("/:id", async (req, res) => {
-  const id = req.params.id;
-  const updatedMeal = req.body;
-  if (isNaN(id) || !valid(updatedMeal, requiredColumns)) {
-    res.status(404).json(` ${id} not valide please provide a number`);
-  } else {
-    try {
-      updatedMeal.id = id;
-      const meal = await knex("meal").where({ id });
-      meal.length === 0
-        ? res.status(200).json(`meal not found`)
-        : (await knex("meal").where({ id }).update(updatedMeal)) &&
+  .put(async (req, res) => {
+    const id = req.params.id;
+    const updatedMeal = req.body;
+    if (isNaN(id) || !valid(updatedMeal, requiredColumns)) {
+      res.status(404).json(` not valide please provide a the right info`);
+    } else {
+      try {
+        updatedMeal.id = id;
+        const meal = await knex("meal").where({ id });
+        if (meal.length === 0) res.status(200).json("meal not found");
+        else {
+          await knex("meal").where({ id }).update(updatedMeal);
           res
             .status(200)
-            .json(`meal ${updatedMeal.title} updated successfully`);
-    } catch (err) {
-      res.status(500).json(err);
+            .json("Meal ${updatedMeal.title} updated successfully");
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
     }
-  }
-});
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  if (isNaN(id)) {
-    res.status(404).json(` ${id} not valide please provide a number`);
-  } else {
-    try {
-      const meal = await knex("meal").where({ id });
-      meal.length === 0
-        ? res.status(200).json(`meal not found`)
-        : (await knex("meal").where({ id }).del()) &&
+  })
+  .delete(async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id) || !id) {
+      res.status(404).json(` ${id} not valide please provide a number`);
+    } else {
+      try {
+        const meal = await knex("meal").where({ id });
+        if (meal.length === 0) res.status(200).json(`meal not found`);
+        else {
+          await knex("meal").where({ id }).del();
           res.status(200).json(`meal with id ${id} deleted successfully`);
-    } catch (err) {
-      res.status(500).json(err);
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
     }
-  }
-});
+  });
 
 module.exports = router;
--- src/backend/api/reservations.js
+++ src/backend/api/reservations.js
@@ -11,86 +11,94 @@ const requiredColumns = [
   "contact_name",
   "contact_email",
 ];
-router.get("/", async (req, res) => {
-  try {
-    const reservations = await knex("reservation");
-    res.status(200).json(reservations);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-router.post("/", async (req, res) => {
-  const addReservation = req.body;
-  if (!valid(addReservation, requiredColumns)) {
-    return res.status(404).json("Missing required columns in the request body");
-  } else {
+router
+  .route("/")
+  .get(async (req, res) => {
     try {
-      const meal = await knex("reservation").insert(addReservation);
-      res
-        .status(201)
-        .json(
-          `reservation ${addReservation.contact_name} with id ${meal} added successfully`
-        );
+      const reservations = await knex("reservation");
+      res.status(200).json(reservations);
     } catch (err) {
       res.status(500).json(err);
     }
-  }
-});
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  if (isNaN(id)) {
-    res.status(404).json(` ${id} not valide please provide a number`);
-  } else {
-    try {
-      const reservation = await knex("reservation").where({ id });
-      reservation.length === 0
-        ? res.status(200).json("no matching reservation")
-        : res.status(200).json(reservation);
-    } catch (err) {
-      res.status(500).json(err);
+  })
+  .post(async (req, res) => {
+    const addReservation = req.body;
+    if (!valid(addReservation, requiredColumns)) {
+      return res
+        .status(404)
+        .json("Missing required columns in the request body");
+    } else {
+      try {
+        const meal = await knex("reservation").insert(addReservation);
+        res
+          .status(201)
+          .json(
+            `reservation ${addReservation.contact_name} with id ${meal} added successfully`
+          );
+      } catch (err) {
+        res.status(500).json(err);
+      }
     }
-  }
-});
-router.put("/:id", async (req, res) => {
-  const id = req.params.id;
-  const updatedReservation = req.body;
-  if (isNaN(id) || !valid(updatedReservation, requiredColumns)) {
-    res.status(404).json(` ${id} not valide please provide a number`);
-  } else {
-    try {
-      updatedReservation.id = id;
-      const reservation = await knex("Reservation").where({ id });
-      reservation.length === 0
-        ? res.status(200).json(`Reservation not found`)
-        : (await knex("Reservation")
-            .where({ id })
-            .update(updatedReservation)) &&
+  });
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id) || !id) {
+      res.status(404).json(` ${id} not valide please provide a number`);
+    } else {
+      try {
+        const reservation = await knex("reservation").where({ id });
+        reservation.length === 0
+          ? res.status(200).json("no matching reservation")
+          : res.status(200).json(reservation);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  })
+  .put(async (req, res) => {
+    const id = req.params.id;
+    const updatedReservation = req.body;
+    if (isNaN(id) || !id || !valid(updatedReservation, requiredColumns)) {
+      res.status(404).json(` ${id} not valide please provide a the right info`);
+    } else {
+      try {
+        updatedReservation.id = id;
+        const reservation = await knex("Reservation").where({ id });
+        if (reservation.length === 0) {
+          res.status(200).json(`Reservation not found`);
+        } else {
+          await knex("Reservation").where({ id }).update(updatedReservation);
           res
             .status(200)
             .json(
               `Reservation with id ${updatedReservation.id} updated successfully`
             );
-    } catch (err) {
-      res.status(500).json(err);
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
     }
-  }
-});
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  if (isNaN(id)) {
-    res.status(404).json(` ${id} not valide please provide a number`);
-  } else {
-    try {
-      const reservation = await knex("reservation").where({ id });
-      reservation.length === 0
-        ? res.status(200).json(`reservation not found`)
-        : (await knex("reservation").where({ id }).del()) &&
+  })
+  .delete(async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id) || !id) {
+      res.status(404).json(` ${id} not valide please provide a number`);
+    } else {
+      try {
+        const reservation = await knex("reservation").where({ id });
+        if (reservation.length === 0) {
+          res.status(200).json(`Reservation not found`);
+        } else {
+          await knex("reservation").where({ id }).del();
           res
             .status(200)
             .json(`reservation with id ${id} deleted successfully`);
-    } catch (err) {
-      res.status(500).json(err);
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
     }
-  }
-});
+  });
 module.exports = router;
