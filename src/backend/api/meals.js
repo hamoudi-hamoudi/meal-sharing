dd37e8d349f13a88e9a00aaf35e4e85493fbac9c
hamoudi-hamoudi <mr-hamoudi@outlook.com>
2023-06-19 19:48:24 +0200

    nodejs week3 Meal sharing endpoints

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 const valid = require("./controller");
+const {
+  maxprice,
+  availableReservations,
+  mealTitle,
+  mealsDates,
+  limitedMeals,
+  sortingMeals,
+  mealReviews,
+} = require("./advancedquerys");
 const requiredColumns = [
   "title",
   "description",
@@ -11,13 +20,49 @@ const requiredColumns = [
   "created_date",
   "price",
 ];
-
 router
   .route("/")
   .get(async (req, res) => {
+    let query = req.query;
+    let meals;
     try {
-      const meals = await knex("meal").select("title");
-      res.status(200).json(meals);
+      if (Object.keys(query).length !== 0) {
+        if ("maxprice" in query) {
+          const amount = Number(query.maxprice);
+          meals = await maxprice(amount);
+        }
+        if ("availableReservations" in query) {
+          const boolean = query.availableReservations.toLowerCase();
+          meals = await availableReservations(boolean);
+        }
+        if ("title" in query) {
+          const searchedTitle = query.title;
+          meals = await mealTitle(searchedTitle);
+        }
+        if ("dateAfter" in query || "dateBefore" in query) {
+          const data = Object.entries(query);
+          meals = await mealsDates(data[0][0], data[0][1]);
+        }
+        if ("limit" in query) {
+          const limit = query.limit;
+          meals = await limitedMeals(Number(limit));
+        }
+        if ("sortKey" in query) {
+          const column = query.sortKey;
+          const sortBy = query.sortDir;
+          if (sortBy) {
+            meals = await sortingMeals(column, sortBy);
+          } else {
+            meals = await sortingMeals(column);
+          }
+        }
+        return meals.length === 0
+          ? res.status(200).json("no meal found")
+          : res.status(200).json(meals);
+      } else {
+        meals = await knex("meal").select("title");
+        res.status(200).json(meals);
+      }
     } catch (err) {
       res.status(500).json(err);
     }
@@ -93,5 +138,19 @@ router
       }
     }
   });
-
+router.get("/:meal_id/reviews", async (req, res) => {
+  const id = req.params["meal_id"];
+  if (isNaN(id) || !id || id === "meal_id") {
+    return res.status(404).json(` ${id} not valide please provide a number`);
+  } else {
+    try {
+      const review = await mealReviews(id);
+      return review.length === 0
+        ? res.status(200).json("no meal found or review found")
+        : res.status(200).json(review);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+});
 module.exports = router;
---
+++ src/backend/api/reviews.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../database");
+const valid = require("./controller");
+
+const requiredCollumns = [
+  "title",
+  "description",
+  "meal_id",
+  "stars",
+  "created_date",
+];
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    const reviews = await knex("*").from("review");
+    res.send(reviews);
+  })
+
+  .post(async (req, res) => {
+    const addReview = req.body;
+    if (!valid(addReview, requiredCollumns)) {
+      return res.status(404).json(` please provide the right collumns info`);
+    } else {
+      try {
+        await knex("review").insert(addReview);
+        res.status(201).json(`review added successfully`);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  });
+
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id) || !id) {
+      res.status(404).json(` ${id} not valide please provide a number`);
+    } else {
+      try {
+        const review = await knex("review").where({ id });
+        review.length === 0
+          ? res.status(200).json("no matching review")
+          : res.status(200).json(review);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  })
+  .put(async (req, res) => {
+    const id = req.params.id;
+    const updatedreview = req.body;
+    if (isNaN(id) || !id || !valid(updatedreview, requiredCollumns)) {
+      res.status(404).json(` ${id} not valide please provide a the right info`);
+    } else {
+      try {
+        updatedreview.id = id;
+        const review = await knex("review").where({ id });
+        if (review.length === 0) {
+          res.status(200).json(`review with this id not found`);
+        } else {
+          await knex("review").where({ id }).update(updatedreview);
+          res
+            .status(200)
+            .json(`review with id ${updatedreview.id} updated successfully`);
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  })
+  .delete(async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id) || !id) {
+      res.status(404).json(` ${id} not valide please provide a number`);
+    } else {
+      try {
+        const review = await knex("review").where({ id });
+        if (review.length === 0) {
+          res.status(200).json(`review not found`);
+        } else {
+          await knex("review").where({ id }).del();
+          res.status(200).json(`review with id ${id} deleted successfully`);
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  });
+
+module.exports = router;
